0f51b7e018aebf2a7c5f85ba42e640febfa5e7be
navin-varu <navin.varu31@gmail.com>
2020-07-25 01:58:07 +0530

    DatabaseContextSetup class added.
    AddDatabseContext extension method added.

---
+++ Bajo/Extensions/DatabaseContextSetup.cs
@@ -0,0 +1,24 @@
+﻿using Bajo.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bajo.Extensions
+{
+    public static class DatabaseContextSetup
+    {
+        public static void AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddDbContextPool<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+    }
+}
